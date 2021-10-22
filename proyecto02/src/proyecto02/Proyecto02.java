294c3dd025713a7bdee15ecab63bf7c314755df0
Joe Churo <jmchuro@utpl.edu.ec>
2021-10-22 11:32:28 -0500

    primer commit

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author jooec
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo dos basico de java");
+        System.out.println("versionado a travez de Java");
+        System.out.println("Manejado desde GitHub");
+        System.out.println("Ademas de usar GitHubClassroom");
+    }
+    
+}
